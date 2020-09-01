9a929c7839bccc319fd2f24a0d650755dcdb3b75
Prince Agrawal <princeagrawalnew123@gmail.com>
2020-09-01 15:45:59 +0530

    added functionality to send mails

--- backend/routes/api/formData.js
+++ backend/routes/api/formData.js
@@ -1,42 +1,65 @@
+require('dotenv').config();
 const express = require("express");
 const router = express.Router();
-const { check, validationResult } = require('express-validator');
+const { check, validationResult } = require('express-validator')
+const nodemailer = require('nodemailer');
 
 const User = require('../../models/users');
 
 router.get('/', (req,res) => res.send("form data route"));
 
 router.post(
-	'/',
-	[
-		check('name', 'Name is required').not().isEmpty(),
-		check('email', 'Please include a valid email').isEmail()
-	],
-	async (req, res) => {
-		const errors = validationResult(req);
-		if (!errors.isEmpty()) {
-		  return res.status(400).json({ errors: errors.array() });
-		}
+  '/',
+  [
+    check('name', 'Name is required').not().isEmpty(),
+    check('email', 'Please include a valid email').isEmail()
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
 
-		const { name, email, pnumber } = req.body;
+    const { name, email, pnumber } = req.body;
 
-		try {
-			let user = await User.findOne({ email });
-			if (user) {
-				return res.status(400).json({ msg: 'Already sent' })
-			}
-			user = new User({
-				name,
-				email,
-				pnumber
-			});
-			await user.save();
-			res.send('Data sent')
-		} catch (err) {
-			console.error(err.message);
-			res.status(500).send('Server error');
-		}
-	}
+    try {
+      let user = await User.findOne({ email });
+      if (user) {
+        return res.status(400).json({ msg: 'Already sent' })
+      }
+      user = new User({
+        name,
+        email,
+        pnumber
+      });
+      await user.save();
+      
+      const transporter = nodemailer.createTransport({
+        host: 'smtp.ethereal.email',
+        port: 587,
+        auth: {
+            user: process.env.username,
+            pass: process.env.password
+        }
+      });
+      
+      let info = await transporter.sendMail({   
+            from: `${email}`,
+            to: `admin@gmail.com, ${email}`,
+            subject: `${name} has contacted`, 
+            html: `Your mail have been sent` 
+      });
+
+      console.log("Message sent: %s", info.messageId);
+      console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+      
+      res.send('Data saved and mails sent...')
+
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server error');
+    }
+  }
 );
 
 module.exports = router; 
