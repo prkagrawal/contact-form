475ceb7a7ec269f981604c4dc9e8e4aab5e64105
Prince Agrawal <princeagrawalnew123@gmail.com>
2020-09-01 13:35:04 +0530

    data validation and saving it to database

--- backend/routes/api/formData.js
+++ backend/routes/api/formData.js
@@ -1,6 +1,42 @@
 const express = require("express");
 const router = express.Router();
+const { check, validationResult } = require('express-validator');
+
+const User = require('../../models/users');
 
 router.get('/', (req,res) => res.send("form data route"));
 
-module.exports = router;
+router.post(
+	'/',
+	[
+		check('name', 'Name is required').not().isEmpty(),
+		check('email', 'Please include a valid email').isEmail()
+	],
+	async (req, res) => {
+		const errors = validationResult(req);
+		if (!errors.isEmpty()) {
+		  return res.status(400).json({ errors: errors.array() });
+		}
+
+		const { name, email, pnumber } = req.body;
+
+		try {
+			let user = await User.findOne({ email });
+			if (user) {
+				return res.status(400).json({ msg: 'Already sent' })
+			}
+			user = new User({
+				name,
+				email,
+				pnumber
+			});
+			await user.save();
+			res.send('Data sent')
+		} catch (err) {
+			console.error(err.message);
+			res.status(500).send('Server error');
+		}
+	}
+);
+
+module.exports = router; 
